e52a6709efb980f0c795d5c8b180e348c04cd6b9
Nithi593 <nithish.present@gmail.com>
2019-04-12 20:02:33 +0530

    addition of all the values in an array

---
+++ arrayadd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a[1005],n,sum,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+     {
+       scanf("%d",&a[i]);
+      }
+    sum=0;
+    for(i=0;i<n;i++)
+     {
+       sum=sum+a[i];
+      }
+    printf("%d",sum);
+}
+
